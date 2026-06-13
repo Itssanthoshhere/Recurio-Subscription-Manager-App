d724549f60d2d14cf66af17e333e686da60a92b7
Santhosh VS <santhosh02vs@gmail.com>
2026-06-14 00:02:13 +0530

    feat: update app configuration and add EAS build settings

--- app.config.js
+++ app.config.js
@@ -7,6 +7,9 @@ export default {
       ...(appJson.expo?.extra || {}),
       posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
       posthogHost: process.env.POSTHOG_HOST,
+      eas: {
+        projectId: "eb3a1e2a-1684-4084-a2f2-54fe1af14cdc",
+      },
     },
   },
 };
